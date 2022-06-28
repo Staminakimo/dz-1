08451f44ec0e51ecd6c1932cc020731ed657cfac
юлька <stamina_kimo@mail.ru>
2022-06-28 16:56:37 +0300

    решение 4oй задачи

---
+++ z-4/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine ("Введите число:");
+string number = Console.ReadLine();
+int number_N = int.Parse (number);
+int number_N2 = 2;
+for (int i = number_N2; i <= number_N; i=i+2)
+{
+Console.WriteLine (i);
+}
+
